1b5764a5cd7741534fd83d0afc79002bd34490f1
carlodaniele <carlo.daniele@gmail.com>
2026-03-14 15:04:44 +0100

    Corretto elementi immagine

--- process_content.py
+++ process_content.py
@@ -292,7 +292,9 @@ if __name__ == "__main__":
                     gallery_html += '\n<figure class="wp-block-gallery has-nested-images columns-default is-cropped">'
                     for img in uploaded_images:
                         # Usiamo il formato corretto per i blocchi immagine Gutenberg
+                        gallery_html = '\n<!-- wp:image {"lightbox":{"enabled":true}, "id": %s, "sizeSlug": "large", "linkDestination":"none"} -->\n' % str(img["id"])
                         gallery_html += f'\n<figure class="wp-block-image size-large"><img src="{img["url"]}" alt="Nomad" class="wp-image-{img["id"]}"/></figure>'
+                        gallery_html += '\n<!-- /wp:image -->\n'
                     gallery_html += '\n</figure>'
                     gallery_html += '\n<!-- /wp:gallery -->\n'
                 elif len(uploaded_images) == 1:
